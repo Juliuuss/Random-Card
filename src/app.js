0ae5bf1f216378203a01c9476515c77c5c3e667f
Juliuuss <j.esp.ver@gmail.com>
2022-12-21 21:14:41 +0000

    Version Final

--- src/app.js
+++ src/app.js
@@ -2,10 +2,6 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-import { read } from "@popperjs/core";
-
 const simbolos = ["&spades;", "&clubs;", "&hearts;", "&diams;"];
 const numeros = ["A", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
 //let pinta = randomSimbolo();
@@ -15,30 +11,31 @@ const numeros = ["A", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
 //document.getElementById("Numero").innerHTML = randomNumero();
 
 function randomNumero() {
-  let aleatorio = Math.floor(Math.random() * 13);
+  let aleatorio = Math.floor(Math.random() * 14);
   return numeros[aleatorio];
 }
 
 function randomSimbolo() {
-  let aleatorio = Math.floor(Math.random() * 3);
+  let aleatorio = Math.floor(Math.random() * 4);
   return simbolos[aleatorio];
 }
 
-function intercambio() {
-  let random = randomSimbolo();
-  document.getElementById("pintaSuperior").innerHTML = random;
-  document.getElementById("pintaInferior").innerHTML = random;
+function generator() {
+  let randomSim = randomSimbolo();
+  document.getElementById("pintaSuperior").innerHTML = randomSim;
+  document.getElementById("pintaInferior").innerHTML = randomSim;
   let randomNum = randomNumero();
   document.getElementById("Numero").innerHTML = randomNum;
-}
-setInterval(intercambio(), 1000);
-//console.log(randomNumero());
-//console.log(randomSimbolo());
 
-//if (pinta == "&hearts;" || "&diams;") {
-//  document.getElementById("pintaSuperior").style.color = "red";
-//  document.getElementById("pintaInferior").style.color = "red";
-//} else {
-//  document.getElementById("pintaSuperior").style.color = "black";
-//  document.getElementById("pintaInferior").style.color = "black";
-//}
+  if (randomSim === "&hearts;" || randomSim === "&diams;") {
+    document.getElementById("pintaSuperior").style.color = "red";
+    document.getElementById("pintaInferior").style.color = "red";
+  } else if (randomSim === "&spades;" || randomSim === "&clubs;") {
+    document.getElementById("pintaSuperior").style.color = "black";
+    document.getElementById("pintaInferior").style.color = "black";
+  }
+}
+window.onload = function() {
+  generator();
+};
+document.querySelector("#genera").addEventListener("click", generator);
